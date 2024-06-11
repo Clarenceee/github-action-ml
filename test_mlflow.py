e1218b12725bfee498e0f0b2b531c76965ad341f
Clarenceee <75594131+Clarenceee@users.noreply.github.com>
2024-06-11 17:36:37 +0800

    Modify mlflow test code

--- test_mlflow.py
+++ test_mlflow.py
@@ -10,14 +10,14 @@ mlflow.set_tracking_uri('http://localhost:5000')
 experiment_name = "Test-Github-Action"
 experiment = mlflow.get_experiment_by_name(experiment_name)
 if experiment:
-    experiment_id = experiment.experiment_id
-else:
-    experiment_id = mlflow.create_experiment(experiment_name, tags=experiment_tags)
+    experiment_id = experiment.experiment_id\
+
+    current_datetime = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    new_run_name = "Training-"+ current_datetime
     
-# %%
-current_datetime = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-new_run_name = "Training-"+ current_datetime
-with mlflow.start_run(experiment_id=experiment_id, run_name=new_run_name):
-    mlflow.log_param("lr_test", 0.001)
-    mlflow.log_metric("loss_test", 0.1)
-# %%
+    with mlflow.start_run(experiment_id=experiment_id, run_name=new_run_name):
+        mlflow.log_param("lr_test", 0.001)
+        mlflow.log_metric("loss_test", 0.1)
+else:
+    print("Experiment not found")
+    
