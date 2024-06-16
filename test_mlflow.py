49403402f6bd36361bb22f753e9898408cfe0cfd
clarence <clarencechanjh@gmail.com>
2024-06-16 12:19:50 +0800

    Connect to dagshub mlflow

--- test_mlflow.py
+++ test_mlflow.py
@@ -4,15 +4,15 @@ import mlflow
 import dagshub
 from datetime import datetime
 
-DAGSHUB_TOKEN = os.environ['dagshub_token']
-print(DAGSHUB_TOKEN)
+# DAGSHUB_TOKEN = os.environ['dagshub_token']
+# print(DAGSHUB_TOKEN)
 
 # %%
 print("Setting Tracking URI")
 
-# mlflow.set_tracking_uri('http://192.168.1.17:5000')
+mlflow.set_tracking_uri('https://dagshub.com/Clarenceee/github-action-ml.mlflow/')
 # dagshub.init("github-action-ml", "Clarenceee", mlflow=True)
-dagshub.auth.add_app_token(DAGSHUB_TOKEN)
+# dagshub.auth.add_app_token('f3874a38d72fbe0ff4ff316dba160cb01cce861f', url='https://dagshub.com/Clarenceee/github-action-ml.mlflow/')
 
 # %%
 experiment_name = "Test-Github-Action"
@@ -25,9 +25,10 @@ if experiment:
     new_run_name = "Training-"+ current_datetime
 
     with mlflow.start_run(experiment_id=experiment_id, run_name=new_run_name):
-        mlflow.log_param("lr_test", 0.002)
-        mlflow.log_metric("loss_test", 0.1)
+        mlflow.log_param("lr_test", 0.001)
+        mlflow.log_metric("loss_test", 0.2)
 else:
     print("Experiment not found")
     
 # Transfer artifacts file out
+# %%
